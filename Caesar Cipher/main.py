2c99a112393e28f7d32e829e15440cfdde0a6964
Jeongsihwa <ghsmsl6@gmail.com>
2026-01-27 21:58:19 +0900

    Caesar Cipher 2

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -21,4 +21,27 @@ def encrypt(original_text, shift_amount):
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
-encrypt(original_text=text, shift_amount=shift)
+
+
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+def decrypt(original_text, shift_amount):
+    decrypted_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        decrypted_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {decrypted_text}")
+
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+
+def caesar():
+    if direction == "encode":
+        encrypt(text, shift)
+    else:
+        decrypt(text, shift)
+
+caesar()
+
